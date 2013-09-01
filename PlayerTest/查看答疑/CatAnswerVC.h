e91159d825f511a8ecaccd1ad28cdcd039c4464a
窦静轩 <dronaldo80@gmail.com>
2013-09-01 20:46:57 +0800

    修改

---
+++ PlayerTest/查看答疑/CatAnswerVC.h
@@ -0,0 +1,13 @@
+//
+//  CatAnswerVC.h
+//  PlayerTest
+//
+//  Created by doujingxuan on 13-9-1.
+//  Copyright (c) 2013年 doujingxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CatAnswerVC : UIViewController
+
+@end
---
+++ PlayerTest/查看答疑/CatAnswerVC.m
@@ -0,0 +1,38 @@
+//
+//  CatAnswerVC.m
+//  PlayerTest
+//
+//  Created by doujingxuan on 13-9-1.
+//  Copyright (c) 2013年 doujingxuan. All rights reserved.
+//
+
+#import "CatAnswerVC.h"
+
+@interface CatAnswerVC ()
+
+@end
+
+@implementation CatAnswerVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
